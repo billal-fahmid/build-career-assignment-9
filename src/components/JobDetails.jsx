8c8ecc9f27ce1c33d6cb56e1a6004da55600128b
billal-fahmid <billalcooom@gmail.com>
2023-04-10 23:44:05 +0600

    details page data add design done

--- src/components/JobDetails.jsx
+++ src/components/JobDetails.jsx
@@ -1,5 +1,5 @@
 import React, { useContext } from 'react';
-import { useParams } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 import { FeaturesContext } from '../App';
 
 const JobDetails = () => {
@@ -10,18 +10,33 @@ const JobDetails = () => {
    const details= features.find(detail => detail.id == id.id);
    console.log(details)
     return (
-        <div className='px-16'>
+        <div className='px-16 py-32'>
             <h1 className='text-center text-[#1A1919] text-5xl font-semibold py-6 mb-16'>Job Details</h1>
-            <div className='flex'>
-                <div className="w-2/3 ">
-                    <p>Job Description: <span>{details.job_description}</span></p>
-                    <p>Job Responsibility: <span>{details.job_responsibility}</span></p>
-                    <p>Educational Requirements:</p>
-                    <span>{details.educational_requirement}</span>
-                    <p>Experiences:</p>
+            <div className='flex mt-12'>
+                <div className="w-2/3">
+                    <p className='mb-6'><span className='font-bold'>Job Description:</span> {details.job_description}</p>
+                    <p className='mb-6'> <span className='font-bold'>Job Responsibility:</span>{details.job_responsibility}</p>
+                    <p className='font-bold'>Educational Requirements:</p>
+                    <span className='mb-6'>{details.educational_requirement}</span>
+                    <p className='font-bold mt-6'>Experiences:</p>
                     <span>{details.experiences}</span>
                 </div>
-                <div className='w-1/3'></div>
+                <div className='w-1/3 '>
+                    <div className='bg-blue-50 p-8'>
+                        <h3 className='text-xl font-bold text-gray-900 border-gray-400 border-b-2 pb-6'>Job Details</h3>
+                        <hr />
+                        <p className='text-xl mt-6 mb-4'><span className='font-semibold'>Salary :</span> {details.salary_range}</p>
+
+                        <p className='text-xl mb-6'><span className='font-semibold'>Job Title :</span > {details.job_title}</p>
+
+                        <h3 className='text-xl font-semibold text-gray-900 border-gray-400 border-b-2 pb-6 mb-6'>Contact Information</h3>
+                        <p className='text-xl mb-6'> <span className='font-semibold'>Phone :</span> {details.phone}</p>
+                        <p className='text-xl mb-6'><span className='font-semibold'>Email :</span > {details.email}</p>
+                        <p className='text-xl mb-6'><span className='font-semibold'>Address :</span> {details.location}</p>
+                        
+                    </div>
+                     <button className='btnn w-full mt-4'>Apply Now</button>
+                </div>
             </div>
         </div>
     );
