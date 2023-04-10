ae09bf4a0a8c4187ce6a043a091434e2dfac930a
billal-fahmid <billalcooom@gmail.com>
2023-04-10 19:08:10 +0600

    job category section is complate

--- src/components/JobCategory.jsx
+++ src/components/JobCategory.jsx
@@ -11,13 +11,15 @@ const JobCategory = () => {
 
     return (
         <div className='mt-32'>
-            <h1 className='text-center text-[#1A1919] text-5xl font-semibold py-4'>Job Category List</h1>
-            <p className='text-center mb-8'>Explore thousands of job opportunities with all the information you need. Its your future</p>
-            <div>
+            <h1 className='text-center text-[#1A1919] text-5xl font-semibold py-6'>Job Category List</h1>
+            <p className='text-center mb-10'>Explore thousands of job opportunities with all the information you need. Its your future</p>
+            <div className='grid md:grid-cols-2 lg:grid-cols-4 justify-between gap-6 mb-16'>
                 {
                     jobs.map(job => (
-                        <div>
-                            <img src={job.logo} alt="" />
+                        <div className='h-[240px]  bg-violet-100 p-8 rounded-lg'>
+                            <img className='h-16 bg-violet-200 p-4'  src={job.logo} alt="" />
+                            <h2 className='text-xl text-gray-800 font-semibold pt-8'>{job.jobSector}</h2>
+                            <p className='pt-2 text-gray-800'>{job.availableJobs}</p>
                         </div>
                     ))
                 }
