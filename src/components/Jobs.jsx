8a087ea1900e3ec77b8a72e092ea135e67779101
billal-fahmid <billalcooom@gmail.com>
2023-04-11 20:18:03 +0600

    chart done

--- src/components/Jobs.jsx
+++ src/components/Jobs.jsx
@@ -35,11 +35,11 @@ const Jobs = () => {
                     apply2.map(job => (
                         <div className='p-7 bg-slate-100 rounded-lg flex'>
                             <img className='h-48 w-48 bg-white rounded-lg' src={job.company_logo} alt="" />
-                            <div className='flex justify-around items-center'>
+                            <div className='flex justify-between items-center'>
                                 <div className='ml-8 w-[500px]'>
                                     <h2 className='text-2xl font-bold mb-2'>{job.job_title}</h2>
                                     <p className='text-xl font-semibold mb-4'>{job.company_name}</p>
-                                    <div className='flex'>
+                                    <div className='flex gap-4'>
                                         <button className='outline-btn'>{job.job_type}</button>
                                         <button className='outline-btn'>{job.type}</button>
                                     </div>
@@ -48,7 +48,7 @@ const Jobs = () => {
                                         <p className='flex items-center font-semibold'><CurrencyDollarIcon className='h-5 w-5 mr-2'></CurrencyDollarIcon> {job.salary_range}</p>
                                     </div>
                                 </div>
-                                <div className=''>
+                                <div className='ml-36'>
                                     <Link to={`/details/:${job.id}`} className='btnn'>View Details</Link>
                                 </div>
                             </div>
--- src/components/Statistics.jsx
+++ src/components/Statistics.jsx
@@ -1,9 +1,80 @@
 import React from 'react';
+// import React, { PureComponent } from 'react';
+import {
+    ComposedChart,
+    Line,
+    Area,
+    Bar,
+    XAxis,
+    YAxis,
+    CartesianGrid,
+    Tooltip,
+    Legend,
+    Scatter,
+    ResponsiveContainer,
+} from 'recharts';
 
 const Statistics = () => {
+    const data = [
+        {
+            id: 1,
+            marks: 60,
+            assignment: 'Build your new year new mission website',
+        },
+        {
+            id: 2,
+            marks: 59,
+            assignment: 'Create a responsive G3-Architects website',
+        },
+        {
+            id: 3,
+            marks: 60,
+            assignment: 'CSS Framework Ed Tech landing page assignment',
+        },
+        {
+            id: 4,
+            marks: 60,
+            assignment: 'Basic Javascript Problem Solving assignment',
+        },
+        {
+            id: 5,
+            marks: 59,
+            assignment: 'Geometry Genius Assignment requirements and private repo',
+        },
+        {
+            id: 6,
+            marks: 60,
+            assignment: 'AI Universe API and ES6 related Assignment',
+        },
+        {
+            id: 7,
+            marks: 60,
+            assignment: ' Make Quiz Hero a great quiz website',
+        },
+    ];
     return (
-        <div>
-            <h2>Statistics page</h2>
+        <div className='px-6 py-16 '>
+            <h1 className='text-center text-[#1A1919] text-5xl font-semibold py-6 mb-16'>Assignment Marks</h1>
+            <div className='mx-auto'>
+                <ComposedChart
+                    width={800}
+                    height={400}
+                    data={data}
+                    margin={{
+                        top: 20,
+                        right: 20,
+                        bottom: 20,
+                        left: 20,
+                    }}
+                >
+                    <CartesianGrid stroke="#f5f5f5" />
+                    <Area dataKey="marks"></Area>
+                    <XAxis dataKey="id" ></XAxis>
+                    <Tooltip></Tooltip>
+                    <YAxis dataKey=""></YAxis>
+                    <Scatter dataKey="assignment"></Scatter>
+                </ComposedChart>
+            </div>
         </div>
     );
 };
